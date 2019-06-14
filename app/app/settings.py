f833781f661fd2c240efcfb19172f90a31f64b41
zohaib younis <zabi.jarral08@gmail.com>
2019-06-14 16:51:34 +0200

    User Registration and Token Getting implemented

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
