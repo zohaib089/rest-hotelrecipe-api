fd0a838f45eac881ce126005ee2b2f2297d03d6a
zohaib younis <zabi.jarral08@gmail.com>
2019-06-15 13:52:40 +0200

    Image field and upload image function added

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
     path('api/hotelrecipe/', include('hotelrecipe.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
